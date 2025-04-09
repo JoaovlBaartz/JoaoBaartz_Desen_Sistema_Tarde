630ab786e25f11986fee53d6c3c91cac57873589
JoaovlBaartz <joaovlbaartz@gmail.com>
2025-04-09 13:45:37 -0300

    Formualrio com script

--- exemplojavascript/formulario.js
+++ exemplojavascript/formulario.js
@@ -24,7 +24,7 @@ function telefone(variavel){
 
 //mascar do Rg e CPF
 function RGeCPF(variavel){
-    variavel=variavel.replace(/\d/g,"")//remove tudo que nao e numero
+    variavel=variavel.replace(/\D/g,"")// remove tudo que nao e digito
     variavel=variavel.replace(/(\d{3})(\d)/,"$1.$2")//coloca um ponto apos o terceiro digito e o quarto
     variavel=variavel.replace(/(\d{3})(\d)/,"$1.$2")//coloca um ponto apos o sexto digito e o setimo
     variavel=variavel.replace(/(\d{3})(\d{1,2})$/,"$1-$2")//coloca o hifem apos o setimo digito e permite apenas 2 digitos
@@ -33,15 +33,15 @@ function RGeCPF(variavel){
 
 //mascara do cep 
 function cep(variavel){
-    variavel=variavel.replace(/\D/g,"")// remove tudo que nao e digito
-    variavel=variavel.replace(/(\{2})(\d)/,"$1.$2")//coloca a segunda barra
-    variavel=variavel.replace(/(\{3})(\d{1,3})$/,"$1-$2")//coloca a segunda barra
-    return variavel
+    variavel = variavel.replace(/\D/g, ""); // remove tudo que não é dígito
+    variavel = variavel.replace(/(\d{2})(\d)/, "$1.$2"); // coloca o ponto após os 2 primeiros dígitos (se desejado)
+    variavel = variavel.replace(/(\d{3})(\d{1,3})$/, "$1-$2"); // coloca outro ponto após os 3 dígitos seguintes
+    return variavel;
 }
 //mascara data
 function data(variavel){
     variavel=variavel.replace(/\D/g,"")//remove tudo que nao e numero
-    variavel=variavel.replace(/(\d{3})(\d)/,"$1/$2")
+    variavel=variavel.replace(/(\d{2})(\d)/,"$1/$2")
     variavel=variavel.replace(/(\d{2})(\d)/,"$1/$2")
     return variavel
 }
