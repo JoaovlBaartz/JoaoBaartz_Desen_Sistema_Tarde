f8a4bdd04fa085c5bcbc479da81332988462d71a
JoaovlBaartz <joaovlbaartz@gmail.com>
2025-04-15 15:31:10 -0300

    PHP aula

---
+++ PHP/aula 2.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+        <?php
+           // Define o fuso horário
+           date_default_timezone_set('America/Sao_Paulo');
+
+           // Pega a data atual
+           $data_hoje = date("d/m/y", time());
+
+           // Pega a hora atual
+           $hora_agora = date("H:i:s", time());
+        ?>
+        
+        <p align="center">Hoje é dia <?php echo $data_hoje; ?></p>
+        <p align="center">Agora são <?php echo $hora_agora; ?></p>
+        
+        <?php
+            echo "<h2 align='center'>João Vitor Luçolli Baartz</h2>";
+        ?>
+    </body>
+</html>
