88bcd2384d1953ca32cf37410668c7e426b03512
JoaovlBaartz <joaovlbaartz@gmail.com>
2025-04-15 17:03:22 -0300

    Create exemploarray.php

---
+++ PHP/exemploarray.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array</title>
+</head>
+<body>
+    <?php
+    $dias = array('domigo','segunda','terça','quarta','quinta','sexta','sabado');
+    echo $dias[1]."<br/>";
+    print_r($dias);
+    echo "<br/>";
+    var_dump($dias);
+    ?>
+        <?php
+            echo " <h2 align='center'>
+                   João Vitor Luçolli Baartz
+                </h2>";
+
+        ?>
+</body>
+</html>
