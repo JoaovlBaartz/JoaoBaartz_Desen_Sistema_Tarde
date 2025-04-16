17164dc1652bdf3cc7c6115f585e6ee958f73219
JoaovlBaartz <joaovlbaartz@gmail.com>
2025-04-16 15:25:34 -0300

    PHP aula

---
+++ PHP/foreach.php
@@ -0,0 +1,13 @@
+<?php
+  $cores = array ("amarelo", "vermelho","verde","azul");
+  foreach($cores as $cor)
+  {
+     echo $cor."</br>";
+  }
+?>
+     <?php
+        echo " <h2 align='center'>
+               João Vitor Luçolli Baartz
+               </h2>";
+
+    ?>
